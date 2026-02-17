44fb7b259235669243b93c3d3c1ece22e1d9d57f
Dunny <duapuy@gmail.com>
2026-02-17 11:18:56 -0600

    Even or odd

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,48 +1,39 @@
 from brain_games import cli
+import prompt  
 import random
-import prompt
-
-question = 0
-name = ''
-expected_answer = ''
-game_over = False
-counter = 0
 
 def main():
-    global game_over, name
-
+    
     print('Welcome to the Brain Games!')
     name = cli.welcome_user()
-    print('"yes" if the number is even, otherwise answer "no"')
-    while game_over == False:
-        if counter == 3: # game won
-            print(f'Congratulations, {name}!')
-            return
-        run_question()
-        run_answer()
+    print ('Answer "yes" if the number is even, otherwise answer "no".')
 
-def run_question():
-    global question, expected_answer
-    
-    question = random_number()
-    print(f'Question: {question}')
-    
-    if question % 2 == 0:
-        expected_answer = 'yes'
+    ask_question(name)
+    ask_question(name)
+    ask_question(name)
+
+    print (f'Congratulations, {name}!') 
+
+def ask_question(name):
+    num1 = random_number()
+    answer = even_number(num1)
+    print (f'Question: {num1}')
+    user_input = prompt.string('Your answer: ')
+    if user_input == answer:
+        print ('Correct!')
     else:
-        expected_answer = 'no'
+        print (f"'{user_input}' is wrong answer ;(. Correct answer was '{answer}'.")
+        print (f"Let's try again, {name}!")    
+        quit()
 
-def run_answer():
-    global name, game_over, counter
-    
-    your_answer = prompt.string('Your answer: ')
-    if your_answer == expected_answer: # correct answer
-        print('Correct!')
-        counter += 1
-    else: # wrong answer
-        print(f'{your_answer} is wrong answer ;(. Correct answer was {expected_answer}.')
-        print(f'Let´s try again, {name}!')
-        game_over = True
+
 
 def random_number():
     return random.randint(1, 100)
+    
+def even_number(num1):
+    if num1%2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+    
